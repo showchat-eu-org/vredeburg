16d633a594bc7ca8fa34ced6bb7ece9fa93131d9
KiwiChat <52504536+KiwiChat@users.noreply.github.com>
2024-02-15 13:51:56 +0100

    Update site.js

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'ShowChat',
   description: 'ShowChat Blog',
-  keywords: ['eleventy', 'template', 'simple', 'clean'],
+  keywords: ['showchat', 'showchat romania', 'romaniachat', 'radioclick'],
   url: 'https://showchat.netlify.app', // your site url without trailing slash
   paginate: 6 // how many posts you want to show for each page
   // uncomment the next line if you want to add disqus to your site
